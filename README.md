9432d5ca76bceb1caedd8543b52af8fe6059fa88
zschreier <zjschreier@gmail.com>
2023-10-06 16:08:09 -0500

    final commit for the moment, fixed final issues in js file, formatted with css file, and updated README file

---
+++ README.md
@@ -0,0 +1,16 @@
+# **Module 4 Challenge:** Quiz Game
+
+## Description
+
+The goal of this project was to create an interactive quiz game that dynamically created and displayed questions and answers. After answering a question, the user would see that the previous question disappears, and the next one shows up. Incorrect questions would steal time from a timer, and at the end of it all, the user would be prompted to save their score and a scoreboard would be populated using the local storage. 
+
+The main language used in this project was JavaScript, as the intent was to create/change most of the webpage dynamically using event listeners. Event bubbling and targeting parent elements were key ideas that were needed to accomplish this goal.
+
+## Preview
+
+The project can be found at [this address](https://github.io/ZSchreier/quiz-game).
+
+The following three screenshots show the start of the quiz, one of the questions, and an example scoreboard:
+![An image of the starting display](./assets/start-page.png)
+![An image of a question being displayed](./assets/question-page.png)
+![An image of the ending scoreboard](./assets/end-page.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 
+// Here are my query selector variables, used for eventListener targeting and dynamically adding the various elements needed
 var questionArea = document.querySelector(".question-area");
 var buttonArea = document.querySelector(".button-area");
 var timerArea = document.querySelector(".timer-area");
@@ -7,6 +8,7 @@ var restartArea = document.querySelector(".restart-area");
 
 var startButton = document.querySelector(".start");
 
+// These are global variable that will be changed throughout the quiz that will affect the endgame state and local storage
 var gameOver = false;
 var questionCurrent = 0;
 var pickedQ = null;
@@ -14,8 +16,11 @@ var secondsLeft = 121;
 
 var newInitials = ""
 var endScore = 0;
-var highscore = [];
 
+// Shoutout to TA Katy for helping with the usage of "||" to make sure I can properly load/read the local storage; didn't realize 'or' works outside of if statements and the like
+var highscore = JSON.parse(localStorage.getItem('highscore')) || [];
+
+// This array of objects is the bread and butter of the code, storing the questions, answers, and the position of the correct answer
 var questionOption = [
   {
     question: "What is the airspeed velocity of an unladen swallow?",
@@ -31,9 +36,15 @@ var questionOption = [
     question: "What year is it?",
     answers: ["2023", "2022", "2024", "I don't know"],
     correct: 0
+  },
+  {
+    question: "What time do the tornado sirens get tested on the first Wednesday of the month in Minnesota?",
+    answers: ["12:00 pm", "1:00 pm", "2:00 pm", "3:00 pm"],
+    correct: 1
   }
 ]
 
+// This controls the timer and when it should click off
 function timer() {
   var timerInterval = setInterval(function() {
     secondsLeft--;
@@ -42,6 +53,7 @@ function timer() {
     if(secondsLeft === 0) {
       clearInterval(timerInterval);
       timerArea.setAttribute("class", "invisible");
+      questionHide();
       quizEnd();
     }else if(gameOver){
       clearInterval(timerInterval);
@@ -51,12 +63,14 @@ function timer() {
   }, 1000);
 }
 
+// This function grabs the next(or first) question when called and adjusts a tracking variable
 function questionPicker() {
   pickedQ = questionOption[questionCurrent]
   questionCurrent++;
   questionPresent();
 }
 
+// This function creates the question and calls another function to make the answer buttons
 function questionPresent() {
   var divQuestion = document.createElement("div")
   
@@ -70,6 +84,7 @@ function questionPresent() {
   }
 }
 
+// This is the aforementioned button-generating function
 function buttonMaker(answer, number) {
   var divButton = document.createElement("button");
   divButton.textContent = answer;
@@ -82,6 +97,7 @@ function buttonMaker(answer, number) {
   }
 }
 
+//This function hides the previous question (or question when time runs out). The "if" statement is there to make sure the first question has already been generated due to the timing of when it is called
 function questionHide() {
   if(pickedQ !== null){
     var oldQuestions = questionArea.querySelectorAll(".new");
@@ -94,7 +110,16 @@ function questionHide() {
   }
 }
 
-function hideScoreboard(){
+// This function is called inside of the above function, and it loops through the selected elements and hides them
+function hideLoop(htmlCollection){
+  for(x=0; x < htmlCollection.length; x++){
+    var htmlSpot = htmlCollection[x];
+    htmlSpot.setAttribute("class", "invisible");
+  }
+}
+
+//This function hides the input screen after finishing the quiz
+function hideInputScore(){
   var scoreTitle = scoreArea.querySelectorAll("h2");
   var scoreText = scoreArea.querySelectorAll("textarea");
   var saveButton = scoreArea.querySelectorAll(".save-initials");
@@ -107,15 +132,9 @@ function hideScoreboard(){
   buttonHide.setAttribute("class", "invisible");
 }
 
-function hideLoop(htmlCollection){
-  for(x=0; x < htmlCollection.length; x++){
-    var htmlSpot = htmlCollection[x];
-    htmlSpot.setAttribute("class", "invisible");
-  }
-}
-
+//This function is the function responsible for moving the quiz to the next question, calling the appropriate functions and ending the game if necessary
 function questionNext() {
-  if(questionCurrent > 2) {
+  if(questionCurrent > 3) {
     questionHide();
     quizEnd();
   } else {
@@ -124,11 +143,13 @@ function questionNext() {
   }
 }
 
+//This function sets a global variable to end the timer and sets off the scoreboard-generating process
 function quizEnd() {
   gameOver = true;
   scoreGrabber();
 }
 
+//This function creates and displays the input field for saving your score
 function scoreGrabber(){
   var scoreboardTitle = document.createElement("h2")
   var initialsHere = document.createElement("textarea");
@@ -144,6 +165,7 @@ function scoreGrabber(){
   scoreArea.appendChild(scoreButton);
 }
 
+// This function renders the information saved in local storage to the webpage, as well as creating the "Play Again" and "Reset Scores" buttons
 function renderScoreboard(){
   scoreboard = JSON.parse(localStorage.getItem('highscore'));
   for(x=0; x < scoreboard.length; x++){
@@ -155,11 +177,18 @@ function renderScoreboard(){
   }
   var restartButton = document.createElement("button");
   restartButton.textContent = "Play Again";
-  restartButton.setAttribute("class", "restart")
+  restartButton.setAttribute("class", "restart");
 
   restartArea.appendChild(restartButton);
+
+  var scoreboardReset = document.createElement("button");
+  scoreboardReset.textContent = "Reset Scores";
+  scoreboardReset.setAttribute("class", "score-reset");
+
+  restartArea.appendChild(scoreboardReset);
 }
 
+// This function saves the new scores to the local storage before calling the functions that hide the input and renders the scoreboard
 function saveChanges() {
   var sendCombined = {
     initials: newInitials,
@@ -167,14 +196,18 @@ function saveChanges() {
   }
 
   highscore.push(sendCombined)
-
+  highscore.sort(function(x,y){
+    if(x.score > y.score){
+      return -1;
+    }
+  })
   localStorage.setItem("highscore", JSON.stringify(highscore))
 
-  hideScoreboard();
+  hideInputScore();
   renderScoreboard();
 }
 
-
+// This event listener starts the game off and hides the button
 startButton.addEventListener("click", function(event){
   event.preventDefault();
   startButton.setAttribute("class", "invisible");
@@ -182,6 +215,7 @@ startButton.addEventListener("click", function(event){
   questionNext();
 })
 
+// This event listener checks whether an answer is right or wrong and moves the quiz to the next question (subtracting timer if necessary)
 buttonArea.addEventListener("click", function(event) {
   event.preventDefault();
   if(event.target.matches("button.correct")){
@@ -192,6 +226,7 @@ buttonArea.addEventListener("click", function(event) {
   }
 })
 
+// This event listener targets the button for saving your initials and score, with an if statement checking to make sure no empty strings get passed
 scoreArea.addEventListener("click", function(event){
   event.preventDefault();
   if(event.target.matches(".save-initials")){
@@ -206,8 +241,12 @@ scoreArea.addEventListener("click", function(event){
   }
 })
 
+// This event listener checks for the restart button AND the score reset button
 restartArea.addEventListener("click", function(event){
   if(event.target.matches(".restart")){
     location.reload();
+  }else if(event.target.matches(".score-reset")){
+    localStorage.setItem("highscore", JSON.stringify([]));
+    scoreArea.setAttribute("class", "invisible");
   }
 })
