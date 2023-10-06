14d1cc73f60d3278dcebaad0e4364dde71bb262f
zschreier <zjschreier@gmail.com>
2023-10-05 20:07:23 -0500

    added more logic into the js file, js file is approx. 90% complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,20 @@
 
 var questionArea = document.querySelector(".question-area");
 var buttonArea = document.querySelector(".button-area");
-var timerArea = document.querySelector(".timer");
+var timerArea = document.querySelector(".timer-area");
+var scoreArea = document.querySelector(".score-area");
+var restartArea = document.querySelector(".restart-area");
 
 var startButton = document.querySelector(".start");
-// var button = document.querySelectorAll("button");
 
 var gameOver = false;
 var questionCurrent = 0;
 var pickedQ = null;
 var secondsLeft = 121;
 
-var endScore;
-var highscore;
+var newInitials = ""
+var endScore = 0;
+var highscore = [];
 
 var questionOption = [
   {
@@ -81,9 +83,7 @@ function buttonMaker(answer, number) {
 }
 
 function questionHide() {
-  if(pickedQ === null){
-    alert(`You have 120 seconds, good luck!`);
-  }else {
+  if(pickedQ !== null){
     var oldQuestions = questionArea.querySelectorAll(".new");
     var oldWrongButtons = buttonArea.querySelectorAll(".wrong");
     var oldCorrectButtons = buttonArea.querySelectorAll(".correct");
@@ -92,6 +92,19 @@ function questionHide() {
     hideLoop(oldWrongButtons);
     hideLoop(oldCorrectButtons);
   }
+}
+
+function hideScoreboard(){
+  var scoreTitle = scoreArea.querySelectorAll("h2");
+  var scoreText = scoreArea.querySelectorAll("textarea");
+  var saveButton = scoreArea.querySelectorAll(".save-initials");
+  var titleHide = scoreTitle[0]
+  var textHide = scoreText[0]
+  var buttonHide = saveButton[0]
+
+  titleHide.setAttribute("class", "invisible");
+  textHide.setAttribute("class", "invisible");
+  buttonHide.setAttribute("class", "invisible");
 }
 
 function hideLoop(htmlCollection){
@@ -112,26 +125,65 @@ function questionNext() {
 }
 
 function quizEnd() {
-  alert(`This is the end for you my friend...`);
   gameOver = true;
+  scoreGrabber();
 }
 
-function init() {
-  highscore = JSON.parse(localStorage.getItem("highscore"))
+function scoreGrabber(){
+  var scoreboardTitle = document.createElement("h2")
+  var initialsHere = document.createElement("textarea");
+  var scoreButton = document.createElement("button");
+  scoreButton.setAttribute("class", "save-initials")
+  endScore = secondsLeft;
+
+  scoreboardTitle.textContent = `You finished with a score of ${endScore}! Enter your initials here to save it to the scoreboard!`;
+  scoreButton.textContent = "Save Initials";
+
+  scoreArea.appendChild(scoreboardTitle);
+  scoreArea.appendChild(initialsHere);
+  scoreArea.appendChild(scoreButton);
+}
+
+function renderScoreboard(){
+  scoreboard = JSON.parse(localStorage.getItem('highscore'));
+  for(x=0; x < scoreboard.length; x++){
+    var scoreDisplay = document.createElement("h4");
+    var scoreInitial = scoreboard[x]
+    var scoreNumber = scoreboard[x]
+    scoreDisplay.textContent = `${scoreInitial.initials} finished with a score of ${scoreNumber.score}!`;
+    scoreArea.appendChild(scoreDisplay);
+  }
+  var restartButton = document.createElement("button");
+  restartButton.textContent = "Play Again";
+  restartButton.setAttribute("class", "restart")
+
+  restartArea.appendChild(restartButton);
 }
 
 function saveChanges() {
+  var sendCombined = {
+    initials: newInitials,
+    score: endScore
+  }
+
+  highscore.push(sendCombined)
+
   localStorage.setItem("highscore", JSON.stringify(highscore))
+
+  hideScoreboard();
+  renderScoreboard();
 }
 
 
-startButton.addEventListener("click", function(){
+startButton.addEventListener("click", function(event){
+  event.preventDefault();
   startButton.setAttribute("class", "invisible");
   timer();
   questionNext();
 })
 
 buttonArea.addEventListener("click", function(event) {
+  event.preventDefault();
   if(event.target.matches("button.correct")){
     questionNext();
   }else if(event.target.matches("button.wrong")){
@@ -140,4 +192,22 @@ buttonArea.addEventListener("click", function(event) {
   }
 })
 
-// init();
+scoreArea.addEventListener("click", function(event){
+  event.preventDefault();
+  if(event.target.matches(".save-initials")){
+    var initialsFind = document.querySelector("textarea");
+    var initialsValue = initialsFind.value.trim();
+    if(initialsValue === ""){
+      newInitials = "Anonymous";
+    }else{
+      newInitials = initialsValue;
+    }
+    saveChanges();
+  }
+})
+
+restartArea.addEventListener("click", function(event){
+  if(event.target.matches(".restart")){
+    location.reload();
+  }
+})
